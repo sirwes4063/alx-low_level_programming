f05bbbaca21659be7ae5873cf5680b6670481542
salifu williams eneojo <williamseneojo@gmail.com>
2023-06-17 07:10:31 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /*
  * main - entry point
- * return - always 0 (success)
+ * return - always returns 0 (success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
