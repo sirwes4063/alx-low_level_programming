1a1f63a5a5423248d4dd8c5c07988a53cff6b862
salifu williams eneojo <williamseneojo@gmail.com>
2023-06-20 06:26:10 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * Description: This program demonstrates the usage of the printf() function
+ *              to print a string to the console.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
+
